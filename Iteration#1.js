62b968915a10be4793bae278e7b08e0247dc0bd2
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-03-05 11:51:11 +0100

    chore: Iteration #1 complete

---
+++ Iteration#1.js
@@ -0,0 +1,11 @@
+/* Crea una arrow function que tenga dos parametros a y b y 
+que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
+por consola la suma de los dos parametros. */
+
+let sum = (a = 10,b = 5) => {
+   
+    return a+b
+
+}
+
+console.log(sum(40))
